efe4a954a44a3cdd1bef90a80457ba204a91c77f
Denis Kuzakov <kuzakovdi@hotmail.com>
2023-04-26 21:58:54 +0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM webdevops/php-nginx:8.2-alpine
+
+ENV WEB_DOCUMENT_ROOT=/app/public \
+    DB_HOST=mariadb \
+    DB_PORT=3306 \
+    DB_NAME=glpidb \
+    DB_USER=glpiuser \
+    DB_PASSWORD=glpipass \
+    LDAP_TRUSTED_CERTIFICATES=none \
+    REDIS_HOST=${REDIS_HOST} \
+    REDIS_PORT=${REDIS_PORT} \
+    REDIS_PASS=${REDIS_PASS} \
+    REDIS_DB=${REDIS_DB}
+
+RUN wget -O /glpi.tgz https://github.com/glpi-project/glpi/releases/download/10.0.7/glpi-10.0.7.tgz
+
+COPY init.sh /opt/docker/
+RUN chmod +x /opt/docker/init.sh
+CMD /opt/docker/init.sh ; /opt/docker/bin/service.d/supervisor.sh
---
+++ init.sh
@@ -0,0 +1,85 @@
+FOLDER_WEB=/app
+
+# Extracting glpi
+tar zxf /glpi.tgz -C ${FOLDER_WEB} --strip-components=1 --skip-old-files && chown 1000:1000 -R ${FOLDER_WEB}
+
+# Removing install.php because of not needed
+rm -rf ${FOLDER_WEB}/install/install.php
+
+# Adding php settings
+echo "session.cookie_httponly = On" >> /opt/docker/etc/php/php.webdevops.ini
+sed -i '1d' /opt/docker/etc/nginx/vhost.common.d/10-php.conf
+sed -i '1 i\location ~ ^/index\.php$ {' /opt/docker/etc/nginx/vhost.common.d/10-php.conf
+
+if [[ -z "$REDIS_HOST" || -z "$REDIS_PORT" || -z "$REDIS_PASS" || -z "$REDIS_DB" ]]; then echo "REDIS isn't used or settings were set incorrect";
+else
+    REDIS_IS_USED=true
+    echo "session.save_handler = redis" >> /opt/docker/etc/php/php.webdevops.ini
+    echo "session.save_path = \"tcp://${REDIS_HOST}:${REDIS_PORT}?auth=${REDIS_PASS}&database=${REDIS_DB}\"" >> /opt/docker/etc/php/php.webdevops.ini
+    echo "REDIS settings for PHP session is successfully updated.";
+fi
+
+#Add trusted certificates to "ldap-truststore"
+if [ "$LDAP_TRUSTED_CERTIFICATES" != 'none' ]; then
+    echo "Adding certificates to truststore..."
+    echo "$LDAP_TRUSTED_CERTIFICATES" | base64 -d > /etc/openldap/my-certificates/extra.pem
+fi
+
+
+# Check that the database is available
+echo "Waiting for database to be ready..."
+while ! nc -w 1 $DB_HOST $DB_PORT; do
+    # Show some progress
+    echo -n '.';
+    sleep 1;
+done
+echo -e "\n\nGreat, "$DB_HOST" is ready!"
+# Give it another 3 seconds.
+sleep 3;
+
+
+#Checking for other containers starting
+echo "Waiting for other containers starting..."
+while [ -f "${FOLDER_WEB}/files/glpi_is_starting" ]; do
+    echo '.';
+    sleep 1;
+done
+echo -e "\nGreat, the current container is ready to start!\n";
+touch ${FOLDER_WEB}/files/glpi_is_starting
+
+# Installation
+if [ "${REDIS_IS_USED}" == "true" ]; then
+    cd ${FOLDER_WEB} && php bin/console glpi:cache:configure --dsn=redis://${REDIS_PASS}@${REDIS_HOST}:${REDIS_PORT}/${REDIS_DB} --no-interaction
+fi
+
+#Checking for GLPI is installed and update\check DB if needed
+if [ -f "${FOLDER_WEB}/files/glpi_is_installed" ]; then
+    echo "GLPI is already installed."
+    cd ${FOLDER_WEB} && php bin/console db:configure --db-host=${DB_HOST} --db-port=${DB_PORT} --db-name=${DB_NAME} --db-user=${DB_USER} --db-password=${DB_PASSWORD} --no-interaction
+    cp ${FOLDER_WEB}/files/glpicrypt.key ${FOLDER_WEB}${FOLDER_GLPI}/config/glpicrypt.key
+    if [ "$(php bin/console glpi:database:check | grep OK)" ]; then
+        echo "DB is already actual."
+    else
+        php bin/console glpi:maintenance:enable
+        php bin/console task:unlock -a
+        php bin/console db:update
+        if [ "$(php bin/console glpi:database:check | grep OK)" ]; then
+            php bin/console glpi:maintenance:disable
+            echo "DB is successfully updated."
+        else
+            php bin/console glpi:maintenance:disable
+            rm -rf ${FOLDER_WEB}/files/glpi_is_starting
+            echo "DB is corrupted. Please check and fix DB, run it in glpi-webroot directory: php bin/console glpi:database:check"
+            exit 1
+        fi
+    fi
+else
+    cd ${FOLDER_WEB} && php bin/console db:install --db-host=${DB_HOST} --db-port=${DB_PORT} --db-name=${DB_NAME} --db-user=${DB_USER} --db-password=${DB_PASSWORD} --no-interaction
+    touch ${FOLDER_WEB}/files/glpi_is_installed
+    cp ${FOLDER_WEB}/config/glpicrypt.key ${FOLDER_WEB}/files/glpicrypt.key
+    
+    echo "GLPI is successfully installed."
+fi
+
+#Remove lock file
+rm -rf ${FOLDER_WEB}/files/glpi_is_starting
